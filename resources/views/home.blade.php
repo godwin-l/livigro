5e7b713de0b984df8cf9338ec2466a8c7d5e8e59
alloygodwin1 <34455677+alloygodwin1@users.noreply.github.com>
2018-10-12 19:17:44 +0530

    login register button
    completed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Livigro</title>
+    </head>
+    <body>
+<a href="/login"><button>Login</button></a>
+<a href="/register"><button>Register</button></a>
+    </body>
+</html>
