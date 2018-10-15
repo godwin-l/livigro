6cfea25ad786cf0e60814ce41c088f4639012bc0
arul armstrong <33225342+arularmstrong@users.noreply.github.com>
2018-10-15 04:21:49 -0400

    Update LabController.php

--- app/Http/Controllers/LabController.php
+++ app/Http/Controllers/LabController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session;
+
 use App\model\Labs;
 class LabController extends Controller
 {
@@ -14,7 +14,6 @@ class LabController extends Controller
     }
     public function labList(){
         $labs = Labs::all();
-        Session::put('user', 2);
         return view('lab.list')->with('labs',$labs);
     }
     
